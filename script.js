6d06b0b9e606c31e12806b004c5e2c29509bea6d
Alessandro Tibaldo <aletibo@gmail.com>
2021-04-17 11:35:46 +0200

    2° bonus completato + alcuni bug da sistemare

--- script.js
+++ script.js
@@ -27,7 +27,15 @@ function initVue() {
             this.newGenre.push(this.albums[i].genre)
           }
         }
-      } // genreFilter
+      }, // genreFilter
+
+      sortPerYearOld: function() {
+        this.albums.sort((a,b) => (a.year > b.year) ? 1 : -1);
+      },
+
+      sortPerYearNew: function() {
+        this.albums.sort((a,b) => (b.year > a.year) ? 1 : -1);
+      }
     } // Methods
   })
 }
